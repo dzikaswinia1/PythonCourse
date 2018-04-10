e5caa809bb3bf9ee8f8e944593194baf9b9b7564
Patryk <skowronpatryk04@gmail.com>
2018-04-10 20:17:27 +0200

    Lesson

---
+++ Podstawy/Lekcja1.py
@@ -0,0 +1,30 @@
+##LISTY
+# Deklarowanie Listy []
+array = ["Movies", "Games", "Python"]
+print(array[0])
+print(array[1])
+print(array[2])
+
+#DICTIONARIES - slowniki
+#Deklarowanie slownika {}
+dictionaries = {"name": "Nick", "age": 16, "hobby": "code maker"}
+print(dictionaries["name"])
+print(dictionaries["age"])
+
+#Variables - zmienne
+#Deklaracja zmiennych   // zmienne nazywamy w nastepujacySposob
+string = "This is a string" + " Hello!"
+print(string)
+greetingWorld = "Hello World!"
+print(greetingWorld)
+greetingWorld = greetingWorld.split(" ")[0] #SPLIT dzieli tekst na czesci, uzywajac 0 otrzymamy "Hello" uzywajac 1 otrzymamy "World"
+print(greetingWorld)
+print(greetingWorld + " Nick") #laczymy zmienna z tekstem
+number = 2
+secondNumber = 3
+print(number * secondNumber + secondNumber * number) #Operacje na zmiennych
+check = True
+print(check)
+
+
+
