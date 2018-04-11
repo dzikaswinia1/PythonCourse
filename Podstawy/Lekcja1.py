22bcefc353cacc9a776bcd8eb826420f7443a7a2
Patryk <skowronpatryk04@gmail.com>
2018-04-11 20:31:09 +0200

    :)

--- Podstawy/Lekcja1.py
+++ Podstawy/Lekcja1.py
@@ -57,6 +57,90 @@ def my_function ():
 
 my_function()
 
+#Dodawanie argumentow do fukcji
+
+def second_function(str1, str2):
+    print(str1)
+    print(str2)
+
+second_function("\nThis is argument one", "This is the second argument")
+
+#Argumenty podstawowe
+
+def print_something(name, age):
+    print("\nMy name is ", name, "my age is ", age)
+
+print_something("Nick", 29)
+
+def new_argument(name = "Someone", age = "Unknow"):
+    print("\n My name is ", name, "my age is ", age)
+
+new_argument()
+new_argument("Patryk", 16)
+
+#Slowa kluczowe argumentow
+
+new_argument(age = 27, name = "Nick")
+
+#Nieskoczone argumenty
+#Za pomoca * mozemy dodawac niezliczona ilosc argumentow
+#Petla "for person in people" iteruje nasze podane argumenty.
+
+def print_people(*people):
+    for person in people:
+        print("This person is", person)
+
+print_people("Nick", "Dan", "Jack", "King", "Smiley")
+
+#Zwraacanie wartosci z funkcji
+# Używamy funkcji RETURN (return)
+
+def do_math(num1, num2):
+    return num1 + num2
+
+math1 = do_math(2, 7)
+math2 = do_math(11, 34)
+
+print("First sum is", math1, "and the second sum is", math2)
+
+#Petla IF ELIF ELSE
+
+check = "Hamburger"
+
+if check == False:
+    print("It is false!")
+elif check == "Hamburger":
+    print("Yummm, hamburgers")
+elif check == "Yo":
+    print("Hello!")
+else:
+    print("It is actually equal to true")
+
+#Petla For WHILE
+numbers = [1, 2, 3, 4, 5]
+
+for item in numbers: #Iterujemy i wypisujemy kazdy element tablicy
+    print(item)
+
+names = ["Nick", "Someone", "Patryk"]
+
+for items in names:
+    print("This person name is", items)
+
+#Petla WHILE
+run = True
+current = 1
+
+while run:
+    if current == 10:
+        run = False
+    else:
+        print(current)
+        current +=1
+
+
+
+
 
 
 
